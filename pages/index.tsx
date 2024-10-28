f7b70bf79cf96c8e911f55bceecb6774f2226c71
Rishi Athanikar <rathanikar@justworks.com>
2024-10-27 20:21:00 -0400

    Form title

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import JwegForm from '../components/JwegForm';
 export default function Home() {
   return (
     <div className="container">
-      <h1 className="title">JWEG Entity suggestion for Sales proposal</h1>
+      <h1 className="title">JWEG Entity suggestion for prospects</h1>
       <JwegForm />
     </div>
   );
